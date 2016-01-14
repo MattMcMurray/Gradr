bed46bb3e2a2f64174b906cc84faaee9530d3798
MattMcMurray <mmcmurray@visualspection.com>
2016-01-14 16:13:38 -0600

    Created shell script to set up VM dependencies

---
+++ get_started.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+apt-get -y update
+
+#################################
+## Install Node.js version 4.X ##
+#################################
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+apt-get install -y build-essential
+
+npm install
