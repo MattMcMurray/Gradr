cab196febb3005d0a7a4ea58885fa5d52032f61b
Jorell Linsangan <jorell.linsangan@gmail.com>
2016-01-14 22:35:13 -0600

    Added a working database that contains 2 tables. "School" and "Student"

---
+++ database/databaseTest.js
@@ -0,0 +1,72 @@
+// import and init database packages
+var Sequelize = require('sequelize');
+var sqlite3 = require("sqlite3");
+
+
+//connect to the sqlite database   
+var sequelize = new Sequelize('study_database', 'softeng2', 'thisisencrypted', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+    //Sqlite only
+    storage: './study_database.sqlite'
+});
+
+
+//define the tables
+Student = sequelize.define('student', {
+    firstName: {
+        type: Sequelize.STRING,
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+});
+
+School = sequelize.define('school', {
+    name: {
+        type: Sequelize.STRING,
+    },
+    type: {
+        type: Sequelize.STRING
+    }
+})
+
+//set the relationship
+School.hasMany(Student)
+
+
+//creating the tables and adding entries to them
+sequelize.sync({force: true}).then(function() {
+        
+        School.create({
+            name: "uofm"
+        }).then(function(school) {
+            Student.create({
+                firstName: "jorell",
+                lastName: "linsangan",
+                schoolId: school.dataValues.id
+            }).then(function() {
+              Student.create({
+                    firstName: "joshua",
+                    lastName: "linsangan",
+                    schoolId: school.dataValues.id
+                })  
+            });    
+        })
+        
+
+}).catch(function(error) {
+    console.log("Something went wrong during model synchronization");
+});
+
+
+
+
+
--- server.js
+++ server.js
@@ -4,10 +4,11 @@
 // BASIC SETUP //
 /////////////////
 
-// import and init packages
+// import and init packages and modules
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+require('./database/databaseTest.js')
 
 // config body parser; allows gathering data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,11 +26,27 @@ router.get('/', function (req, res) {
 	res.json({ message: 'Welcome to API'});
 });
 
+
 // more routes for API go here
+
+router.get('/student', function(req, res) {
+	console.log(req.query);
+	Student.findAll({
+		where: req.query
+	}).then(function(student){
+		res.json(student)
+	});
+});
 
 // Register routes
 app.use('/api', router);
 
 // Start the server
-app.listen(port);
-console.log('App running on port ' + port);
+var server = app.listen(port, function() {
+	console.log('App running on localhost port %s', server.address().port);
+});
+
+
+
+
+
