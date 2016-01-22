cc6ef188ef8bf6f1e9465b557e530d8578c20f6b
MattMcMurray <mathieumcmurray@gmail.com>
2016-01-22 18:08:37 +0000

    Created a front facing template to greet users

--- code/server/main.js
+++ code/server/main.js
@@ -1,15 +1,21 @@
-var api = require('./routes/routes.js');
+// our routes
+var api = require('./routes/api.js');
+var pages = require('./routes/index.js');
+
+// npm modules
 var express = require('express');
 var app = express();
 var body_parser = require('body-parser');
 
-app.use(body_parser.urlencoded({ extended: true }));
-app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true })); // tell node how we want to parse form data
+app.use(body_parser.json()); // tell node to use json for form parsing
+app.set('view engine', 'jade'); // tell node to use jade templating
 
 // PORT 80 is reserved for the system; make sure you run 'sudo node main.js'
 var port = process.env.PORT || 80;
 
-app.use('/api', api.router);
+app.use('/api', api.router); // tell node to use routes.js as our /api/ routes
+app.use('/', pages.router); // tell node to use index.js for front facing pages
 
 var server = app.listen(port, function() {
 	console.log("App running on port " + server.address().port);
--- code/server/routes/index.js
+++ code/server/routes/index.js
@@ -1,11 +1,9 @@
 var _express = require('express');
-
 var router = _express.Router();
 
 router.get("/", function (req, res) {
-	res.json({message: 'Hello world!'});
+    res.render('../templates/index', {});
 });
 
-
-
 module.exports = {router};
+
