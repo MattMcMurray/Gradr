2e70bc77cfe8c105b13331a8bfaa5dbfb92178ed
KeaneTK <keane.tk@gmail.com>
2016-01-27 12:11:53 -0600

    Expanded user to contain more info and started a Likes table.

---
+++ code/server/models/Likes.js
@@ -0,0 +1,46 @@
+var Sequelize = require("sequelize");
+var connection = require("../database.js")
+
+LikeConnection = connection.define('likes', {
+	username: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	targetUsername: {
+		type: Sequelize.STRING,
+		allowNUll: false
+	}, 
+	status: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false
+	}
+});
+
+LikeConnection.sync()
+
+var createLike = function(username, targetUsername, status) {
+	
+	return LikeConnection.create({
+		username: username,
+		targetUsername: targetUsername,
+		status: status
+	});
+}
+
+var getAllLikes = function() {
+	return LikeConnection.findAll();
+}
+
+var getUserLikes = function(username) {
+	return LikeConnection.findAll({
+		where: {
+			username: username
+		}
+	});
+}
+
+module.exports = {
+	createLike: createLike,
+	getAllLikes: getAllLikes,
+	getUserLikes: getUserLikes
+}
--- code/server/models/User.js
+++ code/server/models/User.js
@@ -10,6 +10,49 @@ UserConnection = connection.define('users', {
 	password: {
 		type: Sequelize.STRING,
 		allowNull: false
+	}, 
+	firstname: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	lastname: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	//Did we want to split the address into address/country/city, have all three fields in one string, or have no address at all and just rely on school
+	address: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	city: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	country: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	school: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	courses: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	//A string for just information not captured by the other fields.
+	generalDescription: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	//A string for describing what courses/subject you are looking for help with
+	helpDescription: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}, 
+	dateOfBirth: {
+		type: Sequelize.DATE,
+		allowNull: false
 	}
 });
 
@@ -24,15 +67,27 @@ var getUser = function(username) {
 	});
 }
 
-var createUser = function(username, password) {
+var createUser = function(username, password, firstname, lastname, address, city, country, school, courses, generalDescription, helpDescription, dateOfBirth) {
 	
 	return UserConnection.create({
 		username: username,
-		password: password
+		password: password, 
+		firstname: firstname,
+		lastname: lastname,
+		address: address,
+		city: city,
+		country: country,
+		school: school,
+		courses: courses,
+		generalDescription: generalDescription,
+		helpDescription: helpDescription,
+		dateOfBirth: dateOfBirth
 	});
 }
 
-
+var getAllUsers = function() {
+	return UserConnection.findAll();
+}
 
 module.exports = {
 	getUser: getUser,
