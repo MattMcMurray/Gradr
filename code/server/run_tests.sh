9566dd6a3f2cef94b374dfe980ac8ba66b9b6b17
Matt <mathieumcmurray@gmail.com>
2016-02-01 18:55:55 -0600

    Updated first test, added test running script, updated .gitignore

---
+++ code/server/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+node main.js & # make sure that the file 'study_database.sqlite' exists 
+pkill node # kill the node process; mocha restarts it and will fail if there's something bound to port 80
+sqlite3 study_database.sqlite < testusers.sql # fill the database with test data
+mocha
+
--- code/server/test/api_endpoint_tests.js
+++ code/server/test/api_endpoint_tests.js
@@ -1,6 +1,10 @@
 var app = require('../main.js');
 var request = require('supertest');
-var assert = require('assertthat');
+var assert = require('assertthat'); // View README for documentation https://github.com/thenativeweb/assertthat
+
+//////////////////////////////////////////
+// ALL API TESTS SHOULD GO IN THIS FILE // 
+//////////////////////////////////////////
 
 describe('api', function() {
   // This is just a description, not the actual route the test will use
@@ -12,13 +16,23 @@ describe('api', function() {
       .expect('Content-Type', 'application/json; charset=utf-8') // self explanatory
       .end(function(err, res) {
         if (err) done(err); // exit if there's an error
-          console.log(res.body);
-          assert.that(res.body.users).is.not.null();
-          //res.body.should.have.property('users');
-          // res.body.username.should.have.property('username', 'npeters3t'); // change as appropriate
-          //assert.equal(res.body.users.username, 'npeters3t');
+          assert.that(res.body.user).is.not.null(); // assert the key 'user' is in the json response
+          assert.that(res.body.user.username).is.equalTo("npeters3t");
+          assert.that(res.body.user.firstname).is.equalTo("Nicole");
+          assert.that(res.body.user.lastname).is.equalTo("Peters");
+          assert.that(res.body.user.city).is.equalTo("Dauphin");
           done();
       });
     });
   });
+
+  // PUT NEXT API TESTS HERE:
+
+  // describe('GET /api/blah', function(){
+  //    it('say what it does here', function() {
+  //
+  //      // assertions go here
+  //
+  //    });
+  // });
 });
