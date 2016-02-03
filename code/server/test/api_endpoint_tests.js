b0076db15e86ddd28a2410f59fb9a5af6483efd9
Caleb <maverickmueller@gmail.com>
2016-02-03 15:59:02 -0600

    fixed getMatches and tests for finding and failing to find matches

--- code/server/test/api_endpoint_tests.js
+++ code/server/test/api_endpoint_tests.js
@@ -9,55 +9,53 @@ var assert = require('assertthat'); // View README for documentation https://git
 describe('api', function() {
     // This is just a description, not the actual route the test will use
     describe('GET /api/getUser', function() {
-    it('requests a user from the api', function(done) {
-        request(app)
-        .get('/api/getUser?user=npeters3t') // change to one of the users actually in the test db data
-        .expect(200) 
-        .expect('Content-Type', 'application/json; charset=utf-8') // self explanatory
-        .end(function(err, res) {
-            if (err) done(err); // exit if there's an error
-            assert.that(res.body.user).is.not.null(); // assert the key 'user' is in the json response
-            assert.that(res.body.user.username).is.equalTo("npeters3t");
-            assert.that(res.body.user.firstname).is.equalTo("Nicole");
-            assert.that(res.body.user.lastname).is.equalTo("Peters");
-            assert.that(res.body.user.city).is.equalTo("Dauphin");
-            done();
+        it('requests a user from the api', function(done) {
+            request(app)
+            .get('/api/getUser?user=npeters3t') // change to one of the users actually in the test db data
+            .expect(200) 
+            .expect('Content-Type', 'application/json; charset=utf-8') // self explanatory
+            .end(function(err, res) {
+                if (err) done(err); // exit if there's an error
+                assert.that(res.body.user).is.not.null(); // assert the key 'user' is in the json response
+                assert.that(res.body.user.username).is.equalTo("npeters3t");
+                assert.that(res.body.user.firstname).is.equalTo("Nicole");
+                assert.that(res.body.user.lastname).is.equalTo("Peters");
+                assert.that(res.body.user.city).is.equalTo("Dauphin");
+                done();
+            });
         });
     });
-  });
 
   // PUT NEXT API TESTS HERE:
 
     describe ('GET /api/getPotentialMatches', function() {
         it('requests a list of users that are a match for a provided userID', function(done) {
-            var agent = request(app);
-            agent
-            .post('/api/likeUser')
-            .send({ liker_id: 1, likee_id: 2})
+            request(app)
+            .get('/api/getPotentialMatches')
+            .send({'userId': 1})
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function(done) {
-                agent
-                .post('/api/likeUser')
-                .send({ liker_id: 2, likee_id: 1})
-                .expect(200)
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .end(function(done) {
-                    agent
-                    .get('/api/getPotentialMatches')
-                    .send({'userId': 1})
-                    .expect(200)
-                    .expect('Content-Type', 'application/json; charset=utf-8')
-                    .end(function(err, res) {
-                        if (err) done(err); // exit if there's an error
-                        assert.that(res.body.matches).is.not.null();
-                        assert.that(res.body.matches.length).is.equalTo(1);
-                        done();
-                    });
-                });
+            .end(function(err, res) {
+                if (err) done(err); // exit if there's an error
+                assert.that(res.body.matches).is.not.null();
+                assert.that(res.body.matches.length).is.equalTo(1);
+                assert.that(res.body.matches[0].id).is.not.null();
+                assert.that(res.body.matches[0].id).is.equalTo(2);
                 done();
-            }); 
-            done();
+            });
+        });
+        it('requests a list of matches when none exist', function(done) {
+            request(app)
+            .get('/api/getPotentialMatches')
+            .send({userId: 3})
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .end(function(err, res) {
+                if(err) done(err);
+                assert.that(res.body.matches).is.not.null();
+                assert.that(res.body.matches.length).is.equalTo(0);
+                done();
+            });
         });
     });
 
