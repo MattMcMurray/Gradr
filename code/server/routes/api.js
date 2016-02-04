711e97d01a809db5f7fde5a96b3b514a70bb2b78
MattMcMurray <mathieumcmurray@gmail.com>
2016-02-03 22:01:53 -0500

    Started work on integration testing

--- code/server/routes/api.js
+++ code/server/routes/api.js
@@ -11,14 +11,17 @@ router.get('/', function (req, res) {
 });
 
 router.post('/NewUser', function (req, res) {
-	User.createUser(getCredentials(req)).then(function(data){
-		console.log('New user ' + req.body.username + ' created');
-		res.json({url:"/", message: 'New user created'});
-	}).catch(function(error){
-		res.status(500);
-		res.json(error);
-	});
-	
+    if (req.body.password === req.body.confirmPassword) {
+        User.createUser(getCredentials(req)).then(function(data){
+            console.log('New user ' + req.body.username + ' created');
+            res.json({url:"/", message: 'New user created'});
+        }).catch(function(error){
+            res.status(500);
+            res.json(error);
+        });
+    } else {
+        res.status(400).json({'msg': 'Passwords do not match'});
+    }
 });
 
 router.post('/ProfileUpdate', function (req, res) {
---
+++ code/server/test/integration_tests.js
@@ -0,0 +1,42 @@
+var app = require('../main.js');
+var request = require('supertest');
+var assert = require('assertthat'); // View README for documentation https://github.com/thenativeweb/assertthat
+
+//////////////////////////////////////////////////
+// ALL INTEGRATION TESTS SHOULD GO IN THIS FILE // 
+//////////////////////////////////////////////////
+var username = "mynewusername";
+var password = "hunter2";
+var wrongPassword = "drowssap";
+
+describe('api', function() {
+    describe('GET /api/getUser', function() {
+        it('Create valid user and log in', function(done) {
+            // first make a request with mismatching passwords to make sure server responds appropriately
+            request(app)
+            .post('/api/newUser')
+            .type('form')
+            .send({'username': username, 'password': password, 'confirmPassword': wrongPassword})
+            .expect(400)
+            .expect('Content-Type', 'application/json; charset=utf-8') // self explanatory
+            .end(function(err, res) {
+                if (err) done(err);
+                assert.that(res.body.msg).is.equalTo('Passwords do not match');
+                done();
+            });
+        
+//      .get('/api/getUser?user=npeters3t') // change to one of the users actually in the test db data
+//      .expect(200) 
+//      .expect('Content-Type', 'application/json; charset=utf-8') // self explanatory
+//      .end(function(err, res) {
+//        if (err) done(err); // exit if there's an error
+//          assert.that(res.body.user).is.not.null(); // assert the key 'user' is in the json response
+//          assert.that(res.body.user.username).is.equalTo("npeters3t");
+//          assert.that(res.body.user.firstname).is.equalTo("Nicole");
+//          assert.that(res.body.user.lastname).is.equalTo("Peters");
+//          assert.that(res.body.user.city).is.equalTo("Dauphin");
+//          done();
+//      });
+        });
+    });
+});
