b031e5e00a6fe4b013caf1b8af87dd3e5c3b1e98
Evan Hunzinger <hnz.evn@gmail.com>
2016-02-19 10:48:51 -0800

    Added additional information onto cards.

--- code/server/public/js/matches.js
+++ code/server/public/js/matches.js
@@ -17,18 +17,25 @@ var populateMatches = function(data) {
 		return;
 	}
 	matches = data.matches;
+	console.log(matches);
 	for (var i = 0; i < matches.length; i++) {
-		createMatchCard(matches[i].firstname + " " + matches[i].lastname);
+		createMatchCard(matches[i].firstname + " " + matches[i].lastname,
+			matches[i].school,
+			matches[i].courses,
+			matches[i].gpa);
 	}
 }
 
-function createMatchCard(fullName) {
+function createMatchCard(fullName, school, courses, gpa) {
 	var cardHTML = 
 		"<div class='col-sm-6 col-md-4'>" +
 			"<div class='thumbnail'>" +
 				"<img src='http://thecatapi.com/api/images/get?format=src&type=jpg&size=small'/>" +
 				"<div class='caption'>" +
 					"<h3>" + fullName + "</h3>" +
+					"<p><b>School: </b>" + school + "</p>" +
+					"<p><b>Classes: </b>" + courses + "</p>" +
+					"<p><b>GPA: </b>4.0</p>" +
 				"</div>" +
 			"</div>" +
 		"</div>";
