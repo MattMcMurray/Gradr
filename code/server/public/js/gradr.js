11be50589b7f11ce5b17f98385777a3360e59908
Evan Hunzinger <hnz.evn@gmail.com>
2016-02-25 15:39:57 -0800

    The selector which checks through the href's in the page now only matches the string to the beginning (so it ignores any query strings in the hrefs).

--- code/server/public/js/gradr.js
+++ code/server/public/js/gradr.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     $('#matchesLink').attr('href', '/matches?user=' + sessionStorage.getItem('user_id'));
-    $('a[href="' + this.location.pathname + '"]').addClass('active');
+    $('a[href^="' + this.location.pathname + '"]').addClass('active');
 });
