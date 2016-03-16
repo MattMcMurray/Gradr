8434642d197f3cf8c029eac2447a87c06ce5731a
Steven Bairos <sbairosnovak@gmail.com>
2016-03-15 20:42:56 -0500

    View ratings works

--- code/android/Gradr/app/src/main/java/com/se2/gradr/gradr/fragments/RateMatchFragment.java
+++ code/android/Gradr/app/src/main/java/com/se2/gradr/gradr/fragments/RateMatchFragment.java
@@ -2,21 +2,35 @@ package com.se2.gradr.gradr.fragments;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.se2.gradr.gradr.MatchListActivity;
 import com.se2.gradr.gradr.R;
+import com.se2.gradr.gradr.Rating;
 import com.se2.gradr.gradr.User;
 import com.se2.gradr.gradr.UserAndImage;
 import com.se2.gradr.gradr.ViewMatchActivity;
+import com.se2.gradr.gradr.helpers.GetRequester;
+import com.se2.gradr.gradr.helpers.JsonConverter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,6 +51,10 @@ public class RateMatchFragment extends Fragment {
     private Integer[] ratingOptions;
 
     private OnFragmentInteractionListener mListener;
+
+    private ArrayList<Rating> ratings;
+    private ListView ratingList;
+    private TextView ratingFloat;
 
     //We call findViewById on this guy
     private View rootView;
@@ -84,6 +102,8 @@ public class RateMatchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_rate_match, container, false);
+        ratingList = (ListView) rootView.findViewById(R.id.rating_list);
+        ratingFloat = (TextView) rootView.findViewById(R.id.average_int);
         populateFields();
         getRatings();
         return rootView;
@@ -107,6 +127,7 @@ public class RateMatchFragment extends Fragment {
     }
 
     public void populateFields() {
+        new GetRatingsTask().execute();
         Spinner ratingSpinner = (Spinner) rootView.findViewById(R.id.rating_spinner);
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(rootView.getContext(),
                  android.R.layout.simple_spinner_item,  ratingOptions);
@@ -127,6 +148,97 @@ public class RateMatchFragment extends Fragment {
 
     public void getRatings() {
         //TODO: GET the ratings for this user. Also, need to add textbox where we'll put their aggregate rating
+    }
+
+
+
+    public class RatingListAdapter extends BaseAdapter {
+        Context context;
+        ArrayList<Rating> ratings;
+        private LayoutInflater inflater = null;
+
+        public RatingListAdapter(Context context, ArrayList<Rating> ratings) {
+            this.context = context;
+            this.ratings = ratings;
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position; //Our ratings don't really have IDs, nor do we really need them
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return ratings.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = inflater.inflate(R.layout.rating_row, null);
+            }
+            Rating rating = ratings.get(position);
+
+            TextView textView = (TextView) view.findViewById(R.id.rating_int);
+            textView.setText("" + rating.getScore());
+
+            textView = (TextView) view.findViewById(R.id.comment);
+            textView.setText(rating.getComment());
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return ratings.size();
+        }
+    }
+
+
+
+
+    public class GetRatingsTask extends AsyncTask<Void, Void, JSONObject> {
+
+        /* We had to do the parsing of the rating object in the post execute because there's multiple
+         * the JSON kind of contains 2 different things; the average rating and the array of comments.
+         * It's ok to do this on the main thread though since we know the JSON won't be very large (max 11 elements)
+         */
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                JSONArray jsonArr = json.getJSONArray("reviews");
+                float average = json.getInt("average");
+                ratings = new ArrayList<Rating>();
+                if (average > 0) { //Otherwise, we leave it as the default "not enough ratings"
+                    ratingFloat.setText("" + average);
+                }
+
+                for (int i = 0; i < jsonArr.length(); i++) {
+                    Rating curr = JsonConverter.ratingFromJson((JSONObject) jsonArr.get(i));
+                    ratings.add(curr);
+                }
+                ratingList.setAdapter(new RatingListAdapter(rootView.getContext(), ratings));
+            } catch (JSONException e) {
+                System.out.println("JSON ERROR - while getting ratings.");
+                System.out.println(e.toString());
+            }
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... params) {
+            String url = getString(R.string.http_address_server) + "/api/getRatings?ratee_id=" + match.getId();
+//            ArrayList<User> users = new ArrayList<User>();
+            JSONObject json = null;
+            try {
+                json = GetRequester.doAGetRequest(url);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                System.out.println("ERROR on getting matches");
+            }
+            return json;
+        }
     }
 
     /**
