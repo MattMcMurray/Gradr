a7572dcf72fb43b153bd9fe25c8f05efbcce1113
Evan Hunzinger <hnz.evn@gmail.com>
2016-03-30 00:36:11 -0700

    Added themes and pictures to SQL scripts and reduced number of users.

--- code/server/dbscripts/testmatches.sql
+++ code/server/dbscripts/testmatches.sql
@@ -1,7 +1,7 @@
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (202, 11, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (11, 202, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (202, 1, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (1, 202, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (202, 10, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (202, 203, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
-insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (203, 202, 1, '2016-02-01 03:23:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (52, 11, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (11, 52, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (52, 1, 1,  '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (1, 52, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (52, 10, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (52, 53, 1, '2016-02-01 03:22:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
+insert into user_matches (liker_id, likee_id, likes, createdAt, updatedAt) values (53, 52, 1, '2016-02-01 03:23:27.833 +00:00', '2016-02-01 03:22:27.833 +00:00');
