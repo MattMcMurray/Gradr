c26fa587d2d19727e72a044b4519e3dbede28907
MattMcMurray <mmcmurray@visualspection.com>
2016-04-02 12:21:44 -0500

    Nightwatch test case for profile images

--- code/server/test/nightwatch/e2e_main_interface_tests.js
+++ code/server/test/nightwatch/e2e_main_interface_tests.js
@@ -315,4 +315,46 @@ module.exports = {
 			.assert.containsText('/html/body/div[2]/table', uknownPlaceUser)
 			.end();
 	},
+
+	'Ensure user image changes are reflected': function (browser) {
+		var user1 = 'test_user_1';
+		var user2 = 'test_user_2';
+		var originalImgSrc = 'https://i.imgur.com/Lew00d7.jpg';
+		var updatedImgSrc = 'https://i.imgur.com/UwW5cCn.jpg';
+
+		browser
+			.url(appURL)
+			.waitForElementVisible('/html/body', 10000)
+			.setValue('//*[@id="username"]', user1)
+			.setValue('//*[@id="password"]', user1)
+			.click('//*[@id="login-form"]/button')
+			.waitForElementVisible('//*[@id="userCard"]', 10000)
+			.click('//*[@id="matchesLink"]')
+			.waitForElementVisible('//*[@id="matchesContainer"]/a/div/div', 100000)
+			.assert.attributeContains('//*[@id="matchesContainer"]/a/div/div/img', 'src', originalImgSrc)
+			.click('//*[@id="logout"]')
+			.waitForElementVisible('/html/body', 10000)
+			.setValue('//*[@id="username"]', user2)
+			.setValue('//*[@id="password"]', user2)
+			.click('//*[@id="login-form"]/button')
+			.waitForElementVisible('//*[@id="userCard"]', 10000)
+			.click('//*[@id="navbar-collapse"]/ul[1]/li[1]/a')
+			.waitForElementVisible('//*[@id="userCard"]', 10000)
+			.click('//*[@id="editButton"]')
+			.clearValue('//*[@id="picture"]')
+			.setValue('//*[@id="picture"]', updatedImgSrc)
+			.moveToElement('/html/body/div[1]/div', 0, 0)
+			.click('//*[@id="editButton"]')
+			.waitForElementVisible('//*[@id="logout"]', 10000)
+			.click('//*[@id="logout"]')
+			.waitForElementVisible('/html/body', 10000)
+			.setValue('//*[@id="username"]', user1)
+			.setValue('//*[@id="password"]', user1)
+			.click('//*[@id="login-form"]/button')
+			.waitForElementVisible('//*[@id="userCard"]', 10000)
+			.click('//*[@id="matchesLink"]')
+			.waitForElementVisible('//*[@id="matchesContainer"]/a/div/div', 100000)
+			.assert.attributeContains('//*[@id="matchesContainer"]/a/div/div/img', 'src', updatedImgSrc)
+			.end();	
+	}
 }
